6cad434bd17638b7126ae340489279e527edd1b4
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-04-21 22:33:54 +0200

    app colors manger

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsManger {
+  static const Color mainColor = Color(0xffEDAB54);
+  static const Color darkBrown = Color(0xff8E4D00);
+  static const Color orange = Color(0xffE98024);
+  static const Color lightOrange = Color(0xffFFEBDA);
+  static const Color gray = Color(0xff9D9D9D);
+  static const Color darkGray = Color(0xff444444);
+}
