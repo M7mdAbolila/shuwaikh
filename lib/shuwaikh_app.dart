7b2e112fa5e6f48648e2850ce66d9843c009cec3
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-04-21 21:56:08 +0200

    add base dependencies

---
+++ lib/shuwaikh_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShuwaikhApp extends StatelessWidget {
+  const ShuwaikhApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
