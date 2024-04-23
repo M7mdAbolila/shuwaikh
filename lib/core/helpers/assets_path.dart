8c51164579f9573264de5f05d69b2d95ab9f57d3
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-04-24 00:17:23 +0200

    part of ui

--- lib/core/helpers/assets_path.dart
+++ lib/core/helpers/assets_path.dart
@@ -4,5 +4,6 @@ class Assets {
   static const String userAvatar = 'assets/icons/user-avatar.svg';
   static const String password = 'assets/icons/password.svg';
   static const String menuIcon = 'assets/icons/menu_icon.svg';
-
+  static const String banner = 'assets/images/Banner.png';
+  static const String broduct = 'assets/images/broduct.png';
 }
--- lib/core/widgets/app_background.dart
+++ lib/core/widgets/app_background.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../helpers/spacing.dart';
 import '../theming/colors.dart';
 import '../theming/styles.dart';
@@ -23,95 +22,89 @@ class AppScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      color: ColorsManager.mainBlue,
-      child: CustomScrollView(
-        slivers: [
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Column(
-              children: [
-                verticalSpace(50),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          InkWell(
-                            onTap: appBarOnPressed,
-                            child: appBarIcon ?? const SizedBox(),
-                          ),
-                          Text(
-                            appBarTitle ?? '',
-                            style: TextStyles.font30White400Weight,
-                          ),
-                          horizontalSpace(1),
-                          isHome
-                              ? InkWell(
-                                  onTap: () {},
-                                  child: const CircleAvatar(
-                                    backgroundColor: ColorsManager.darkBlue,
-                                    child: Icon(
-                                      Icons.notifications_none,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                )
-                              : const SizedBox(),
-                        ],
-                      ),
-                      isSubWidget
-                          ? Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                verticalSpace(20),
-                                Text.rich(
-                                  TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: 'Welcome to \n',
-                                        style: TextStyles.font26White500Weight,
-                                      ),
-                                      TextSpan(
-                                        text: 'KOFEHOLIC',
-                                        style: TextStyles.font36White700Weight,
-                                      ),
-                                    ],
+        width: double.infinity,
+        color: ColorsManager.mainBlue,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              verticalSpace(55),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: appBarOnPressed,
+                          child: appBarIcon ?? const SizedBox(),
+                        ),
+                        Text(
+                          appBarTitle ?? '',
+                          style: TextStyles.font30White400Weight,
+                        ),
+                        horizontalSpace(1),
+                        isHome
+                            ? InkWell(
+                                onTap: () {},
+                                child: const CircleAvatar(
+                                  backgroundColor: ColorsManager.darkBlue,
+                                  child: Icon(
+                                    Icons.notifications_none,
+                                    color: Colors.white,
                                   ),
                                 ),
-                                verticalSpace(20),
-                              ],
-                            )
-                          : Column(
-                              children: [
-                                const SizedBox.shrink(),
-                                verticalSpace(45),
-                              ],
-                            ),
-                    ],
-                  ),
+                              )
+                            : const SizedBox(),
+                      ],
+                    ),
+                    isSubWidget
+                        ? Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              verticalSpace(20),
+                              Text.rich(
+                                TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Welcome to \n',
+                                      style: TextStyles.font26White500Weight,
+                                    ),
+                                    TextSpan(
+                                      text: 'KOFEHOLIC',
+                                      style: TextStyles.font36White700Weight,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              verticalSpace(20),
+                            ],
+                          )
+                        : Column(
+                            children: [
+                              const SizedBox.shrink(),
+                              verticalSpace(45),
+                            ],
+                          ),
+                  ],
                 ),
-                Expanded(
-                  child: Container(
+              ),
+              Column(
+                children: [
+                  Container(
                     width: double.infinity,
-                    decoration: const BoxDecoration(
+                    decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
+                      borderRadius: BorderRadius.circular(30),
                     ),
                     child: body,
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
