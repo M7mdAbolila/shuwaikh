8c491947eb5114d04569ab14bcfa0b96337eca3f
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-04-28 17:52:07 +0300

    add networking layer to project

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  static const String apiBaseUrl = "https://shuwaikhcoffee.com/api/";
+  static const String login = "";
+  static const String signup = "";
+  static const String home = "home-mobile";
+  static const String productsPage = "items-mobile";
+  static const String productDetails = "ProductDetails/";
+  static const String favourites = "products/favourites";
+}
